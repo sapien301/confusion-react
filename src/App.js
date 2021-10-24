f7224e13947d893dcdf0247b8142faa8bc4bfe5b
sapien <joynal@localhost.localdomain>
2021-10-24 11:15:23 +0530

    Components2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-// import logo from './logo.svg';
+import React, { Component } from "react";
+
 import { Navbar,NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  render(){
+  
   return (
+
     <div >
       <Navbar dark color='primary'>
         <div className="container">
@@ -13,9 +26,9 @@ function App() {
 
       </Navbar>
 
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
 }
-
+}
 export default App;
